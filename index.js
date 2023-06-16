88214f04cddaad1c31bf4d9cba53ac80c76d57f1
60342360047-1 <naiiwolf00@gmail.com>
2023-06-17 01:15:02 +0700

    test03

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://fabulous-marshmallow-0befbf.netlify.app"],
     methods: ["GET", "POST"],
   },
 });
