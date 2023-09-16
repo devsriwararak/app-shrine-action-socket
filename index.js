cd3f9acee0d08377012712ecb4ebafce3c8fba84
devsriwararak <devsriwararak.work@gmail.com>
2023-09-16 21:21:52 +0700

    make it better

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://shrine-auction-app.netlify.app",
+      "https://shrine-auction2-app.netlify.app",
       "http://192.168.1.58:5173",
       "http://192.168.43.77:5173",
       "http://192.168.1.149:5173",
